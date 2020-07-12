0891265f1efcefe795ea35882ef883b2008bfa61
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-12 19:37:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,6 +38,6 @@ def adding
 
 
   # return the shipping_manifest hash below
-shipping_manifest["muskets"] => 2
-shipping_manifest["gun powder"] => 4
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 end
